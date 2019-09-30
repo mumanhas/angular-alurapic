ac482a73c79b1d6892a42a46777cfc1c10346b76
Murilo Manhas <murilomanhas@gmail.com>
2019-09-30 19:03:18 -0300

    #pt1 - aula 05

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -7,10 +7,12 @@ const API = 'http://localhost:3000'
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
 
-  constructor(private http: HttpClient){}
+  constructor(
+    private http: HttpClient,
+  ){}
 
   listFromUser(userName){
     return this.http.
-    get<Photo[]>(API + '/flavio/photos')
+    get<Photo[]>(API + '/' + userName + '/photos')
   }
 }
