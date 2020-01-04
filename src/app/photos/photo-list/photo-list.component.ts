8c1edc17c090497ecae249f595f9ca138fedb36f
Murilo <murilomanhas@gmail.com>
2020-01-04 01:51:38 -0300

    #pt1 - final

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 
 @Component({
@@ -17,15 +16,28 @@ export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = "";
   nome: string = "Flavio";
-  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+    ) {}
   
   ngOnInit(): void {
-    this.photos = this.activatedRoute.snapshot.data.photos;
-    this.debounce
-    .pipe(debounceTime(300))
-    .subscribe(filter => this.filter = filter);
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+  }
+
+  load() {
+    this.photoService
+    .listFromUserPaginated(this.userName, ++this.currentPage)
+    .subscribe(photos => {
+      this.filter = '';
+      this.photos = this.photos.concat(photos);
+      if (!photos.length) this.hasMore = false;
+    });
   }
 
 }
